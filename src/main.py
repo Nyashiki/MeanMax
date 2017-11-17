3617c524d24f0b51082f85f9fe742a6d27515c4a
nyashiki <noemail>
2017-11-18 03:24:23 +0900

    最も近いWreckにフルスロットルで移動。 Wood 3 -> Wood 2

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
+import sys
+import math
 import numpy as np
 
 
 
 # Constants
 DEBUG = True
+
+INF = 9999999
 
 REAPER = 0
 WRECK = 4
@@ -13,10 +17,9 @@ REAPER_RADIUS = 400
 
 # Class
 class Reaper:
-    def __init__(self, unit_id, player_id, mass,
+    def __init__(self, unit_id, mass,
                        radius, x, y, vx, vy, extra, extra2):
         self.unit_id = unit_id
-        self.player_id = player_id
         self.mass = mass
         self.radius = radius
         self.x = x
@@ -39,25 +42,39 @@ class Wreck:
 
 # Global
 score = []
-reaper = []
+my_reaper = []
+op_reaper = []
 wreck = []
 
 # Function
 def game_turn_input():
-    global score, reaper, wreck
+    global score, my_reaper, op_reaper, wreck
 
     score = [None for x in range(PLAYER_NUM)]
     for i in range(PLAYER_NUM):
         score[i] = int(input())
+        if DEBUG:
+            sys.stderr.write(str(score[i]) + "\n")
 
     for i in range(PLAYER_NUM):
-        input()
+        x = input()
+        if DEBUG:
+            sys.stderr.write(x + "\n")
     
-    reaper = []
+    my_reaper = []
+    op_reaper = []
     wreck = []
     unit_count = int(input())
+    if DEBUG:
+        sys.stderr.write(str(unit_count) + "\n")
+        
     for i in range(unit_count):
         unit_id, unit_type, player_id, mass, radius, x, y, vx, vy, extra, extra2 = input().split()
+        if DEBUG:
+            sys.stderr.write(unit_id + " " + unit_type + " " + player_id + " " 
+                           + mass + " " + radius + " " + x + " " + y + " "
+                           + vx + " " + vy + " " + extra + " " + extra2 + "\n")
+
         unit_id = int(unit_id)
         unit_type = int(unit_type)
         player_id = int(player_id)
@@ -71,16 +88,42 @@ def game_turn_input():
         extra2 = int(extra2)
 
         if unit_type == REAPER:
-            reaper.append(Reaper(unit_id, player_id, mass, radius, 
-                x, y, vx, vy, extra, extra2))
+            if (player_id == -1):
+                op_reaper.append(Reaper(unit_id, mass, radius, 
+                    x, y, vx, vy, extra, extra2))
+            else:
+                my_reaper.append(Reaper(unit_id, mass, radius, 
+                    x, y, vx, vy, extra, extra2))
+
         elif unit_type == WRECK:
             wreck.append(Wreck(unit_id, mass, radius, x, y, extra, extra2))
     
+
+def dist2D(x1, y1, x2, y2):
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+def think():
+    dist_min = INF
+
+    nx = -1
+    ny = -1
+
+    for w in wreck:
+        dist = dist2D(my_reaper[0].x, my_reaper[0].y, w.x, w.y)
+        if dist < dist_min:
+            dist_min = dist
+            nx = w.x
+            ny = w.y
+
+
+
+    print(nx, ny, 300)        
 
 if __name__ == "__main__":
     # main loop
     while True:
         game_turn_input()
-        print("WAIT")
+        think()
+
         print("WAIT")
         print("WAIT")
